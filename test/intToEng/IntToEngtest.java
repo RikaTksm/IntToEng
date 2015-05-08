c45c42dcd479d1f8552d819dcf2b54a474b56afa
g13935nm <g13935nm@s30429.tsuda.ac.jp>
2015-05-08 12:06:04 +0900

    ドライバー　ryuy ナビゲーター　RikaTksm 0-9までのテストケース作成

---
+++ test/intToEng/IntToEngtest.java
@@ -0,0 +1,65 @@
+package intToEng;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IntToEngtest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
+package intToEng;
+
+import static org.junit.Assert.*;
+
+
+
+import org.junit.Test;
+
+public class test {
+
+	@Test
+	public void test() {
+		public void 0‚©‚ç9‚Ü‚Å(){
+				IntToEng ite= new IntToEng();
+				String expected0="zero";
+				String expected1="one";
+				String expected2="two";
+				String expected3="three";
+				String expected4="four";
+				String expected5="five";
+				String expected6="six";
+				String expected7="seven";
+				String expected8="eight";
+				String expected9="nine";
+				
+				
+				String actual0=ite.translateEng(0);
+				String actual1=ite.translateEng(1);
+				String actual2=ite.translateEng(2);
+				String actual3=ite.translateEng(3);
+				String actual4=ite.translateEng(4);
+				String actual5=ite.translateEng(5);
+				String actual6=ite.translateEng(6);
+				String actual7=ite.translateEng(7);
+				String actual8=ite.translateEng(8);
+				String actual9=ite.translateEng(9);
+				
+				assertThat(actual0,is(expected0));
+				assertThat(actual1,is(expected1));
+				assertThat(actual2,is(expected2));
+				assertThat(actual3,is(expected3));
+				assertThat(actual4,is(expected4));
+				assertThat(actual5,is(expected5));
+		        assertThat(actual6,is(expected6));
+		        assertThat(actual7,is(expected7));
+	            assertThat(actual8,is(expected8));
+	            assertThat(actual9,is(expected9));
+		}
+	}
+}
+               
